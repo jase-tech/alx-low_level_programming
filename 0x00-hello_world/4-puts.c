3c62f042c0ab66920e12e26dbace5009b9dd3418
jase-tech <charitymakwara@gmail.com>
2022-09-12 17:57:07 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle\0\n");
+
+	return (0);
+
+}
